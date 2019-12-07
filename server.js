cdb7ccbf9a7dcc7d719e822121db614c03df7a27
Pablo Moreno <morenop@flip3.engr.oregonstate.edu>
2019-12-07 13:08:24 -0800

    Removes handlebars

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 var path = require('path');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var app = express();
 var port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
